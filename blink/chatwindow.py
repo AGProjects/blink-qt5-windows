622fd3cf7d070349aa68f24b75ea4e04aa8c8ea9
Adrian Georgescu <ag@ag-projects.com>
2025-01-17 21:41:09 +0000

    Capture exception

--- blink/chatwindow.py
+++ blink/chatwindow.py
@@ -2630,6 +2630,9 @@ class ChatWindow(base_class, ui_class, ColorHelperMixin):
     def show_session_info(self):
         self.show()
         self.session_list.hide()
+        if not self.selected_session:
+            return
+
         if self.selected_session.active_panel != self.info_panel:
             if self.sliding_panels:
                 self.session_details.slideInWidget(self.info_panel, direction=self.slide_direction)
